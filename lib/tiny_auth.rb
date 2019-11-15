59559790a782d7aa6828160af68d6e0bd179c542
Ray Zane <raymondzane@gmail.com>
2019-11-15 00:24:07 -0500

    Extract authentication logic

--- lib/tiny_auth.rb
+++ lib/tiny_auth.rb
@@ -1,6 +1,59 @@
 require "tiny_auth/version"
+require "active_record"
+require "globalid"
 
-module TinyAuth
+class TinyAuth
   class Error < StandardError; end
-  # Your code goes here...
+  class PersistError < StandardError; end
+
+  def initialize(model, scope: model)
+    @model = model
+    @scope = scope
+  end
+
+  def find_by_email(email)
+    @scope.find_by(@model.arel_table[:email].lower.eq(email.downcase))
+  end
+
+  def find_by_credentials(email, password)
+    resource = find_by_email(email)
+    resource if resource&.authenticate(password)
+  end
+
+  def generate_token(resource, purpose: :access, expires_in: 24.hours)
+    resource.to_sgid(expires_in: expires_in, for: purpose)
+  end
+
+  def find_by_token(token, purpose: :access)
+    GlobalID::Locator.locate_signed(token, for: purpose)
+  end
+
+  def generate_reset_token(resource, expires_in: 2.hours)
+    update_reset(
+      resource,
+      reset_token: SecureRandom.base58(24),
+      reset_token_expires_at: Time.now + expires_in
+    )
+
+    resource.reset_token
+  end
+
+  def exchange_reset_token(reset_token, changes = {})
+    changes = changes.merge(reset_token: nil, reset_token_expires_at: nil)
+    not_expired = @model.arel_table[:reset_token_expires_at].gt(Time.now)
+    resource = @scope.where(not_expired).find_by(reset_token: reset_token)
+
+    yield resource if resource && block_given?
+    update_reset(resource, changes) if resource
+  end
+
+  private
+
+  def update_reset(resource, changes)
+    if resource.update(changes)
+      resource
+    else
+      raise PersistError, "Failed to reset password."
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,28 @@
 require "bundler/setup"
 require "tiny_auth"
 
+GlobalID.app = 'auth'
+SignedGlobalID.verifier = ActiveSupport::MessageVerifier.new('sekret')
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :email, null: false
+    t.string :password_digest, null: false
+    t.string :reset_token
+    t.datetime :reset_token_expires_at
+    t.index :email, unique: true
+    t.index :reset_token, unique: true
+  end
+end
+
+class User < ActiveRecord::Base
+  include GlobalID::Identification
+  has_secure_password
+  has_secure_token :reset_token
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -10,5 +32,9 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before :each do
+    User.delete_all
   end
 end
